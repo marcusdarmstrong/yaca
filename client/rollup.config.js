f2297ff057574db2afcb614edc0b124d5e69a391
Marcus Armstrong <marcusdarmstrong@gmail.com>
2019-04-05 22:09:42 -0400

    Commas matter

--- client/rollup.config.js
+++ client/rollup.config.js
@@ -12,7 +12,7 @@ export default {
   plugins: [
     replace({
       'process.env.NODE_ENV': JSON.stringify('production')
-    })
+    }),
     resolve(),
     babel({
       exclude: 'node_modules/**' // only transpile our source code
