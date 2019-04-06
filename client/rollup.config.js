f312171702f5394c23539e1c7ed97a6d83b9c7e5
Marcus Armstrong <marcusdarmstrong@gmail.com>
2019-04-05 22:08:46 -0400

    Node env

--- client/rollup.config.js
+++ client/rollup.config.js
@@ -1,6 +1,7 @@
 import resolve from 'rollup-plugin-node-resolve';
 import babel from 'rollup-plugin-babel';
 import commonjs from 'rollup-plugin-commonjs';
+import replace from 'rollup-plugin-replace';
 
 export default {
   input: './client/index.js',
@@ -9,6 +10,9 @@ export default {
     format: 'iife'
   },
   plugins: [
+    replace({
+      'process.env.NODE_ENV': JSON.stringify('production')
+    })
     resolve(),
     babel({
       exclude: 'node_modules/**' // only transpile our source code
