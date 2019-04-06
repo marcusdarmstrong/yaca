4aaed8ad00e543890fad94e7f3ac66c06abcb36a
Marcus Armstrong <marcusdarmstrong@gmail.com>
2019-04-05 21:30:44 -0400

    Maybe it's the getter

--- server.js
+++ server.js
@@ -4,7 +4,17 @@ const SocketServer = require('ws').Server;
 const port = process.env.PORT || 3000;
 const server = express();
 server.set("x-powered-by", false);
-//server.use("/client/", express.static("client/dist"));
+server.use("/client/", express.static("client/dist"));
+server.use((req, res) => {
+  res.send(`<!doctype html><html>
+<head><title>Commenting</title></head>
+<body>
+  <script src="https://yaca-web.herokuapp.com/client/bundle.js"></script>
+</body>
+</html>`);
+});
+server.listen(port, () => console.log(`Yaca listening on port ${port}!`));
+
 
 const wss = new SocketServer({ server });
 
@@ -18,14 +28,3 @@ setInterval(() => {
     client.send(new Date().toTimeString());
   });
 }, 1000);
-
-
-/*server.get("/", (req, res) => {
-  res.send(`<!doctype html><html>
-<head><title>Commenting</title></head>
-<body>
-  <script src="https://yaca-web.herokuapp.com/client/bundle.js"></script>
-</body>
-</html>`);
-});*/
-server.listen(port, () => console.log(`Yaca listening on port ${port}!`));
