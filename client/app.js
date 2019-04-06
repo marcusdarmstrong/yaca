3be256c7cc9c74b0d45d92e0355e026242f15f5c
Marcus Armstrong <marcusdarmstrong@gmail.com>
2019-04-05 22:16:59 -0400

    Backwards logic

--- client/app.js
+++ client/app.js
@@ -2,13 +2,17 @@ import React, { useState, useEffect } from 'react';
 
 export default ({ host, path, socket }) => {
   const [ time, setTime ] = useState(null);
+
   useEffect(() => {
     const listener = event => { setTime(event.data); };
     socket.addEventListener('message', listener);
     return () => socket.removeEventListener('message', listener);
   }, [socket]);
+
+
+
   return (
-    time 
+    !time 
       ? <div>Connecting to the server...</div>
       : <div>The server says the time is {time}</div>
   );
