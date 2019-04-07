58b309ef646c77a134d6be33555ace37caa77c03
Marcus Armstrong <marcusdarmstrong@gmail.com>
2019-04-07 13:15:17 -0400

    Adding a form

--- client/app.js
+++ client/app.js
@@ -61,6 +61,44 @@ const Pingback = React.memo(({ socket }) => {
   );
 });
 
+const guid = () => {
+    let d = new Date().getTime();
+    if (typeof performance !== 'undefined' && typeof performance.now === 'function') {
+        d += performance.now();
+    }
+    return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, c => {
+        let r = (d + Math.random() * 16) % 16 | 0;
+        d = Math.floor(d / 16);
+        return (c === 'x' ? r : (r & 0x3 | 0x8)).toString(16);
+    });
+}
+
+const submit = (username, comment) => {
+  const params = new URLSearchParams();
+  params.username = username;
+  params.comment = comment;
+  params.clientId = guid();
+  params.timestamp = Date.now();
+  fetch(`https://yaca-web.herokuapp.com/api/add-comment`, {
+    method: 'POST',
+    body: params
+  });
+};
+
+const Form = () => {
+  const [ comment, setComment ] = useState("");
+  const [ username, setUsername ] = useState("");
+  return (
+    <div>
+      <form onSubmit={() => submit(username, comment)}>
+        <textarea name="comment" onChange={e => setComment(e.target.value)} />
+        <input type="text" name="username" onChange={e => setUsername(e.target.value)} />
+        <input type="submit" name="submit" value="Add Comment" />
+      </form>
+    </div>
+  );
+}
+
 export default ({ host, path, socket }) => {
   const [ state, setState ] = useState(null);
 
@@ -91,6 +129,7 @@ export default ({ host, path, socket }) => {
   return (
     <>
       <Pingback socket={socket} />
+      <Form />
       {state && state.payload &&
         <Message author={state.payload[0].author} timestamp={new Date()} body={state.payload[0].body} />}
     </>
