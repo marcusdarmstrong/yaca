1d693ef2f13186ef978a588b6c9983c3db4f8b03
Marcus Armstrong <marcusdarmstrong@gmail.com>
2019-12-27 15:06:03 -0500

    Switching to typescript

--- client/rollup.config.js
+++ client/rollup.config.js
@@ -1,29 +1,46 @@
-import resolve from 'rollup-plugin-node-resolve';
+import resolve from '@rollup/plugin-node-resolve';
 import babel from 'rollup-plugin-babel';
-import commonjs from 'rollup-plugin-commonjs';
-import replace from 'rollup-plugin-replace';
+import commonjs from '@rollup/plugin-commonjs';
+import replace from '@rollup/plugin-replace';
 import { terser } from 'rollup-plugin-terser';
+import { eslint } from "rollup-plugin-eslint";
+
+import react from 'react';
+import reactDom from 'react-dom';
 
 export default {
-  input: './client/index.js',
+  input: './client/index.tsx',
   output: {
     file: './client/dist/bundle.js',
     format: 'iife'
   },
   plugins: [
+    eslint({ fix: true, throwOnError: true }),
     replace({
       'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV)
     }),
-    resolve(),
-    babel({
-      exclude: 'node_modules/**' // only transpile our source code
-    }),
+    resolve({ extensions: [".js", ".ts", ".tsx"] }),
     commonjs({
       include: 'node_modules/**',
       namedExports: {
-        'node_modules/react/index.js': ['useState', 'useEffect']
-      }
+        react: Object.keys(react),
+        'react-dom': Object.keys(reactDom),
+      },
     }),
+    babel({
+      babelrc: false,
+      extensions: [".js", ".ts", ".tsx"],
+      exclude: 'node_modules/**', // only transpile our source code
+      presets: [
+        ["@babel/preset-env", {"modules": false}],
+        "@babel/preset-react",
+        "@babel/preset-typescript"
+      ],
+      plugins: [
+        "@babel/plugin-proposal-optional-chaining",
+      ]
+    }),
+
     process.env.NODE_ENV === 'production' ? terser() : null,
   ]
 };
